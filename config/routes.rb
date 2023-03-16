454cbea2a283094124f67484394f2a6c4ff2f00f
Ilie Stavenschi <istavenschi@gmail.com>
2023-03-16 14:53:10 +0200

    add index route and page with all articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
