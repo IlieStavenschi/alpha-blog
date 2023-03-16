41fa3b632a7d9cf2a6a15c4bb6caad6879b9a182
Ilie Stavenschi <istavenschi@gmail.com>
2023-03-16 14:29:25 +0200

    add show method

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
