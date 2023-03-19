1959bc87189f8a462d9e99d359253972c240a073
Ilie Stavenschi <istavenschi@gmail.com>
2023-03-19 13:05:45 +0200

    DRY and add rest of buttons in the views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   # This method finds the article with the ID specified in the params hash
   def show
-    @article = Article.find(params[:id])
   end
   
   # This method retrieves all articles from the database
@@ -15,6 +14,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  end
+
   # This method is used to create a new article by instantiating a new Article object
   # with the parameters from the request
   def create
@@ -24,7 +26,7 @@ class ArticlesController < ApplicationController
     # of the request using the "require" method, which raises an error if the specified parameter is missing
     # The "permit" method is used to specify which parameters are allowed for the article object,
     # in this case, the "title" and "description" attributes
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # This line create a new article in DB based on the input from the form
     if @article.save
       # This line is a rails helper method to display the message after saving
@@ -36,5 +38,30 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-end
 
+  def update
+    @article.update(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully updated."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+    flash[:notice] = "Article was deleted successfully."
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
